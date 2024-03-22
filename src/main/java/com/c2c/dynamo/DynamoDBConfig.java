aa0c4e75476ae8bd6452a2aa4112cf718f07a83b
John Williams <spjohn85@gmail.com>
2024-03-22 10:59:37 +0530

    using default credentials provider

--- src/main/java/com/c2c/dynamo/DynamoDBConfig.java
+++ src/main/java/com/c2c/dynamo/DynamoDBConfig.java
@@ -1,16 +1,12 @@
 package com.c2c.dynamo;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -48,7 +44,7 @@ public class DynamoDBConfig {
     }*/
 
     public AWSCredentialsProvider amazonAWSCredentialsProvider() {
-        return new ProfileCredentialsProvider();
+        return new DefaultAWSCredentialsProviderChain();
     }
 
 }
